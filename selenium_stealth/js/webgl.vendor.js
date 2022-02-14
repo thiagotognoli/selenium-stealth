7799b3867d7a810573cf807faa0adc7d339fb5c8
Thiago Tognoli <public@togger.com.br>
2022-02-14 17:50:23 -0300

    test webgl

--- selenium_stealth/js/webgl.vendor.js
+++ selenium_stealth/js/webgl.vendor.js
@@ -1,10 +1,15 @@
 // https://github.com/berstend/puppeteer-extra/blob/c44c8bb0224c6bba2554017bfb9d7a1d0119f92f/packages/puppeteer-extra-plugin-stealth/evasions/webgl.vendor/index.js
 
 (opts) => {
+  console.log('opts1')
+  console.log(opts)
   const getParameterProxyHandler = {
     apply: function(target, ctx, args) {
       const param = (args || [])[0]
       const result = utils.cache.Reflect.apply(target, ctx, args)
+      console.log('opts==2')
+      console.log(opts)
+      console.log(param)
       // UNMASKED_VENDOR_WEBGL
       if (param === 37445) {
         return opts.vendor || 'Intel Inc.' // default in headless: Google Inc.
