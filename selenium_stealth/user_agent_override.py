a9bb33cedf5470db605c9b8ee3a6b760daf7a076
Thiago Tognoli <public@togger.com.br>
2022-02-14 17:35:14 -0300

    test ua

--- selenium_stealth/user_agent_override.py
+++ selenium_stealth/user_agent_override.py
@@ -16,20 +16,17 @@ def user_agent_override(
     else:
         ua = user_agent
     ua = ua.replace("HeadlessChrome", "Chrome")  # hide headless nature
-    override = {}
-    if language and platform:
-        override = {"userAgent": ua, "acceptLanguage": language, "platform": platform}
-    elif not language and platform:
-        override = {"userAgent": ua, "acceptLanguage": language, "platform": platform}
-    elif language and not platform:
-        override = {"userAgent": ua, "acceptLanguage": language, "platform": platform}
-    else:
-        override = {"userAgent": ua}
     
-    #send(driver, "Network.setUserAgentOverride", override)
-    #send(driver, "Network.setUserAgentOverride", {"source": override})
-    #evaluateOnNewDocument(driver, 'Network.setUserAgentOverride', override)
-
+    # override = {}
+    # if language and platform:
+    #     override = {"userAgent": ua, "acceptLanguage": language, "platform": platform}
+    # elif not language and platform:
+    #     override = {"userAgent": ua, "acceptLanguage": language, "platform": platform}
+    # elif language and not platform:
+    #     override = {"userAgent": ua, "acceptLanguage": language, "platform": platform}
+    # else:
+    #     override = {"userAgent": ua}
+    
     # override["userAgentMetadata"] = driver.execute_script(
     #     'return '+evaluationString(
     #         Path(__file__).parent.joinpath("js/platform.js").read_text(),
@@ -40,17 +37,13 @@ def user_agent_override(
     override = driver.execute_script(
         'return '+evaluationString(
             Path(__file__).parent.joinpath("js/platform.js").read_text(),
-            {"userAgent": user_agent, "language": language, "platform": platform, "maskLinux": False }
+            {"userAgent": ua, "language": language, "platform": platform, "maskLinux": False }
         )
     )
     if language:
         override["acceptLanguage"] = language
      
-    # r = driver.execute_cdp_cmd('() => {"a": 1}', {})
-    # print(f"=================== r = {r}")
-    
     driver.execute_cdp_cmd('Network.setUserAgentOverride', override)
-    
     
     # evaluateOnNewDocument(
     #     driver, Path(__file__).parent.joinpath("js/platform.js").read_text(),
