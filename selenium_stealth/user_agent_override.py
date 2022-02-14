46621415990b96bd1b0dc9351913499c5732c0db
Thiago Tognoli <public@togger.com.br>
2022-02-14 16:50:15 -0300

    teste ua

--- selenium_stealth/user_agent_override.py
+++ selenium_stealth/user_agent_override.py
@@ -29,7 +29,13 @@ def user_agent_override(
     #send(driver, "Network.setUserAgentOverride", override)
     #send(driver, "Network.setUserAgentOverride", {"source": override})
     #evaluateOnNewDocument(driver, 'Network.setUserAgentOverride', override)
-    
+
+    print(        evaluationString(
+            Path(__file__).parent.joinpath("js/platform.js").read_text(),
+            {"opts": {"userAgent": user_agent, "language": language, "platform": platform, "maskLinux": False }}
+        )
+)
+        
     r = driver.execute_script(
         evaluationString(
             Path(__file__).parent.joinpath("js/platform.js").read_text(),
